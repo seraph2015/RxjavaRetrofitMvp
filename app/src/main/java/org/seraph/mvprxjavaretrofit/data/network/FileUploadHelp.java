9ad2c61dc5afabbcc3bc4edd773dd9c6b2830902
seraph2015 <41753393@qq.com>
2017-07-25 18:41:05 +0800

    添加通用的多文件上传

--- app/src/main/java/org/seraph/mvprxjavaretrofit/data/network/FileUploadHelp.java
+++ app/src/main/java/org/seraph/mvprxjavaretrofit/data/network/FileUploadHelp.java
@@ -19,12 +19,46 @@ import okhttp3.RequestBody;
 public class FileUploadHelp {
 
     /**
-     * 多文件表单上传RequestBody
-     * @param params 表单
-     * @param files 文件列表
+     * 多文件表单上传RequestBody(文件统一接收key)
+     *
+     * @param params  表单
+     * @param files   文件list列表
      * @param fileKey 服务器接收文件key
      */
     public static RequestBody multipartRequestBody(Map<String, String> params, List<File> files, String fileKey) {
+        MultipartBody.Builder requestBodyBuilder = initParamsBuilder(params);
+        if (files != null) {
+            for (File file : files) {
+                requestBodyBuilder.addFormDataPart(Tools.isNull(fileKey) ? "file" : fileKey, file.getName(), RequestBody.create(MultipartBody.FORM, file));
+            }
+        }
+        return requestBodyBuilder.build();
+    }
+
+    /**
+     * 多文件表单上传RequestBody
+     *
+     * @param params 表单
+     * @param files  文件map列表
+     */
+    public static RequestBody multipartRequestBody(Map<String, String> params, Map<String, File> files) {
+        MultipartBody.Builder requestBodyBuilder = initParamsBuilder(params);
+        if (files != null) {
+            Set<String> fileKeys = files.keySet();
+            for (String key : fileKeys) {
+                File tempFile = files.get(key);
+                requestBodyBuilder.addFormDataPart(key, tempFile.getName(), RequestBody.create(MultipartBody.FORM, tempFile));
+            }
+        }
+        return requestBodyBuilder.build();
+    }
+
+
+    /**
+     * 初始化和生成公共构建部分
+     * @param params 表单
+     */
+    private static MultipartBody.Builder initParamsBuilder(Map<String, String> params){
         MultipartBody.Builder requestBodyBuilder = new MultipartBody.Builder().setType(MultipartBody.FORM);
         if (params != null) {
             Set<String> paramsKeys = params.keySet();
@@ -32,12 +66,7 @@ public class FileUploadHelp {
                 requestBodyBuilder.addFormDataPart(key, params.get(key));
             }
         }
-        if (files != null) {
-            for (File file : files) {
-                requestBodyBuilder.addFormDataPart(Tools.isNull(fileKey) ? "file" : fileKey, file.getName(), RequestBody.create(MultipartBody.FORM, file));
-            }
-        }
-        return requestBodyBuilder.build();
+        return requestBodyBuilder;
     }
 
 }
