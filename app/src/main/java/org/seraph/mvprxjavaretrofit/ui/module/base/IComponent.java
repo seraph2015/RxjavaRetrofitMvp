6ae37642707b963f07a7f8db558daa06a9f9df75
seraph2015 <417753393@qq.com>
2017-10-24 15:04:52 +0800

    补充注释

--- app/src/main/java/org/seraph/mvprxjavaretrofit/ui/module/base/IComponent.java
+++ app/src/main/java/org/seraph/mvprxjavaretrofit/ui/module/base/IComponent.java
@@ -1,7 +1,7 @@
 package org.seraph.mvprxjavaretrofit.ui.module.base;
 
 /**
- * 获取通用Component
+ * 获取通用Component（为依赖activity的子fragment获取Component而设计）
  * date：2017/7/28 10:54
  * author：xiongj
  * mail：417753393@qq.com
