3769d1e4bb20d138355428833b759e2e0d61f54a
seraph2015 <417753393@qq.com>
2017-12-13 00:59:49 +0800

    部分修改

--- app/src/main/java/org/seraph/mvprxjavaretrofit/AppApplication.java
+++ app/src/main/java/org/seraph/mvprxjavaretrofit/AppApplication.java
@@ -2,6 +2,7 @@ package org.seraph.mvprxjavaretrofit;
 
 import android.app.Application;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.blankj.utilcode.util.Utils;
 import com.tencent.bugly.crashreport.CrashReport;
 
@@ -16,7 +17,7 @@ import org.seraph.mvprxjavaretrofit.di.module.base.AppModule;
  * author：xiongj
  * mail：417753393@qq.com
  **/
-public class AppApplication extends Application {
+public class AppApplication extends Application{
 
     private AppComponent appComponent;
 
@@ -25,23 +26,43 @@ public class AppApplication extends Application {
         super.onCreate();
         initImageLoad();
         initDagger2();
+        //工具初始化
         Utils.init(this);
+        initToastLayout();
+        //腾讯bug日志收集
         CrashReport.initCrashReport(this, "c475f0a560", false);
         //注册activity回调
         registerActivityLifecycleCallbacks(new AppActivityCallbacks());
     }
 
+    /**
+     * 初始化提弹出提示布局资源显示
+     */
+    private void initToastLayout() {
+        ToastUtils.setBgColor(0xFE000000);
+    }
+
+    /**
+     * 初始化Dagger2
+     */
     private void initDagger2() {
         this.appComponent = DaggerAppComponent.builder().appModule(new AppModule(this)).build();
     }
 
+    /**
+     * 初始化图片加载
+     */
     private void initImageLoad() {
         ImageFactory.initPicasso(this);
-      //  ImageFactory.initFresco(this);
+        //  ImageFactory.initFresco(this);
     }
 
+    /**
+     * 获取AppComponent
+     */
     public AppComponent getAppComponent() {
         return appComponent;
     }
+
 
 }
