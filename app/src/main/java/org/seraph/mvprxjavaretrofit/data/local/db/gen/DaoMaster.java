2ed0f8551fa7300509ec7eca713a33a83f812a50
justin <zoubing5213@163.com>
2018-01-05 10:46:59 +0800

    忽略文件

--- app/src/main/java/org/seraph/mvprxjavaretrofit/data/local/db/gen/DaoMaster.java
+++ app/src/main/java/org/seraph/mvprxjavaretrofit/data/local/db/gen/DaoMaster.java
@@ -21,14 +21,14 @@ public class DaoMaster extends AbstractDaoMaster {
 
     /** Creates underlying database table using DAOs. */
     public static void createAllTables(Database db, boolean ifNotExists) {
-        UserTableDao.createTable(db, ifNotExists);
         SearchHistoryTableDao.createTable(db, ifNotExists);
+        UserTableDao.createTable(db, ifNotExists);
     }
 
     /** Drops underlying database table using DAOs. */
     public static void dropAllTables(Database db, boolean ifExists) {
-        UserTableDao.dropTable(db, ifExists);
         SearchHistoryTableDao.dropTable(db, ifExists);
+        UserTableDao.dropTable(db, ifExists);
     }
 
     /**
@@ -47,8 +47,8 @@ public class DaoMaster extends AbstractDaoMaster {
 
     public DaoMaster(Database db) {
         super(db, SCHEMA_VERSION);
-        registerDaoClass(UserTableDao.class);
         registerDaoClass(SearchHistoryTableDao.class);
+        registerDaoClass(UserTableDao.class);
     }
 
     public DaoSession newSession() {
